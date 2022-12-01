357823f5f85ad7433bdb7a9f8c4710698cd0e6fb
Emre Guven <e.emreguven@outlook.com>
2022-12-01 16:19:15 +0300

    Changed category selection style and button locations

--- lib/pages/get_book_page.dart
+++ lib/pages/get_book_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shelf_mobil_frontend/types/category.dart';
 
-
 class GetBookPage extends StatefulWidget {
   const GetBookPage({super.key});
   static final List<String> categoryNameList = Category.getCategoryNameList();
@@ -10,7 +9,7 @@ class GetBookPage extends StatefulWidget {
 }
 
 class _GetBookPageState extends State<GetBookPage> {
-  static String? _selectedCategory = GetBookPage.categoryNameList.elementAt(0);
+  static String _selectedCategory = GetBookPage.categoryNameList.elementAt(0);
 
   @override
   Widget build(BuildContext context) {
@@ -29,13 +28,67 @@ class _GetBookPageState extends State<GetBookPage> {
         ),
         child: Column(
           children: [
+            Container(
+              padding: const EdgeInsets.all(5),
+              height: 50,
+              child: Row(
+                children: [
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectedCategory = "ALL BOOKS";
+                      });
+                    },
+                    style: ButtonStyle(
+                      fixedSize: MaterialStatePropertyAll(
+                          Size(MediaQuery.of(context).size.width * 0.27, 40)),
+                      foregroundColor:
+                          GetBookPage.categoryNameList.elementAt(0) ==
+                                  _selectedCategory
+                              ? const MaterialStatePropertyAll(Colors.white)
+                              : const MaterialStatePropertyAll(
+                                  Color.fromARGB(200, 37, 37, 37),
+                                ),
+                      backgroundColor:
+                          GetBookPage.categoryNameList.elementAt(0) ==
+                                  _selectedCategory
+                              ? const MaterialStatePropertyAll(
+                                  Color.fromARGB(255, 95, 186, 242))
+                              : const MaterialStatePropertyAll(Colors.white),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                    ),
+                    child: const Text("ALL BOOKS"),
+                  ),
+                  const VerticalDivider(
+                    width: 10,
+                    thickness: 1,
+                    indent: 0,
+                    endIndent: 0,
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: GetBookPage.categoryNameList.length - 1,
+                      itemBuilder: _buildCategoryItem,
+                      separatorBuilder: ((context, index) {
+                        return const SizedBox(
+                          width: 10,
+                        );
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             const SizedBox(height: 5),
             Row(
               children: [
                 Flexible(
                   flex: 1,
                   child: SizedBox(
-                    height: 50,
+                    height: 45,
                     child: TextField(
                       textAlignVertical: TextAlignVertical.bottom,
                       cursorColor: Colors.grey,
@@ -57,51 +110,66 @@ class _GetBookPageState extends State<GetBookPage> {
                     ),
                   ),
                 ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  height: 43,
+                  width: 45,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      width: 1.5,
+                      color: const Color.fromARGB(200, 37, 37, 37),
+                    ),
+                  ),
+                  child: IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.sort)),
+                ),
               ],
             ),
             const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  DropdownButton<String>(
-                    menuMaxHeight: MediaQuery.of(context).size.height * 0.4,
-                    value: _selectedCategory,
-                    items: GetBookPage.categoryNameList
-                        .map(
-                          (item) => DropdownMenuItem<String>(
-                            value: item,
-                            child: Text(item),
-                          ),
-                        )
-                        .toList(),
-                    onChanged: ((item) {
-                      setState(() {
-                        _selectedCategory = item;
-                      });
-                    }),
-                  ),
-                  
-                ],
-              ),
-            ),
+            const Divider(),
             const SizedBox(height: 5),
             Expanded(
               child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              childAspectRatio: 0.8,
-                              crossAxisSpacing: 10,
-                              mainAxisSpacing: 10),
-                      itemBuilder: _buildGridItem,
-                      itemCount: 20,
-                    )
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.8,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10),
+                itemBuilder: _buildGridItem,
+                itemCount: 20,
+              ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildCategoryItem(BuildContext context, int index) {
+    return OutlinedButton(
+      onPressed: () {
+        setState(() {
+          _selectedCategory = GetBookPage.categoryNameList.elementAt(index + 1);
+        });
+      },
+      style: ButtonStyle(
+        foregroundColor: GetBookPage.categoryNameList.elementAt(index + 1) ==
+                _selectedCategory
+            ? const MaterialStatePropertyAll(Colors.white)
+            : const MaterialStatePropertyAll(
+                Color.fromARGB(200, 37, 37, 37),
+              ),
+        backgroundColor: GetBookPage.categoryNameList.elementAt(index + 1) ==
+                _selectedCategory
+            ? const MaterialStatePropertyAll(Color.fromARGB(255, 95, 186, 242))
+            : const MaterialStatePropertyAll(Colors.white),
+        shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))),
+      ),
+      child: Text(GetBookPage.categoryNameList.elementAt(index + 1)),
     );
   }
 
@@ -132,6 +200,4 @@ class _GetBookPageState extends State<GetBookPage> {
       ]),
     );
   }
-
-  
 }
