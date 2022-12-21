928b193e0ea20e64327e731ab52e42216da96425
Emirkan Burak Yılmaz <ebylmz17@gmail.com>
2022-12-21 21:43:48 +0300

    Added constants and enums files

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  static String baseUrl =
+      'https://my-json-server.typicode.com/GTU-Ghostbusters/db_test/';
+  static String booksEndpoint = '/books';
+  static String categoryEndpoint = '/categories';
+}
