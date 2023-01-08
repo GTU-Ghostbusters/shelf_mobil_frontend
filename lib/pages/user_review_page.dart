9d9c8d8c5d87086691cd598f9d66ec77cb24a9d3
dogukankryns <dogukankarayunus@gmail.com>
2023-01-09 00:45:05 +0300

    Updated review page

--- lib/pages/user_review_page.dart
+++ lib/pages/user_review_page.dart
@@ -38,9 +38,91 @@ class _UserReviewPageState extends State<UserReviewPage> {
           ),
           []),
       body: Container(
+        padding: const EdgeInsets.all(10),
         decoration: Background().getBackground(),
-        child: Center(
-            child: Text(_reviewList.isEmpty ? "" : _reviewList.first.review)),
+        child: Column(
+          children: [
+            Card(
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  _reviewList.isEmpty
+                      ? "0 review"
+                      : _reviewList.length == 1
+                          ? "1 review"
+                          : "${_reviewList.length} reviews",
+                  style: TextStyle(color: Colors.grey.shade800, fontSize: 15),
+                ),
+              ]),
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: _reviewList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            color: Colors.transparent,
+                            height: MediaQuery.of(context).size.height * 0.15,
+                            width: MediaQuery.of(context).size.width * 0.68,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    child: SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.45,
+                                      child: Text(
+                                        _reviewList.first.review,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.clip,
+                                        style: TextStyle(
+                                            color: Colors.grey.shade800,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: MediaQuery.of(context).size.height *
+                                        0.02,
+                                    child: Text(
+                                      _reviewList.first.buyerId.toString(),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                          color: Theme.of(context).primaryColor,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w900),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(
+                    height: 5,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
